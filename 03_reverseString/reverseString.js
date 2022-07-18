ff5ab3a56beb0b05b63d8837f809e90b4b3efeb4
Travis Ridgway <ridgwaytravis@gmail.com>
2022-07-17 21:35:37 -0500

    Add script to convert string to array, reverse string and rejoin

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const charArray = string.split("");
+    revArray = charArray.reverse().join("");
+    return revArray;
 };
 
 // Do not edit below this line
